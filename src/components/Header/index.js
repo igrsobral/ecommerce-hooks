e82896d2d7f800e80dd983c9d92df0ae27c34279
igor ribeiro <igor_1917@hotmail.com>
2020-03-13 18:15:08 -0300

    home and cart page converted with hooks

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>{cartSize} itens</span>
+          <MdShoppingBasket size={36} color="#fff" />
+        </div>
+      </Cart>
+    </Container>
+  );
+}
